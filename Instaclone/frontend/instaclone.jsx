07c337e336c16aa32e5ab487932a27ef6e0c975b
Maxine Chui <maxine.chui@gmail.com>
2017-11-21 21:04:20 -0800

    Added login/signup links

--- Instaclone/frontend/instaclone.jsx
+++ Instaclone/frontend/instaclone.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { login, signup, logout } from './util/session_api_util';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>What's happening</h1>, root);
-  console.log("in doc ready");
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
+  ReactDOM.render(<Root store={ store }/>, root);
+  // console.log("in doc ready");
+  // window.login = login;
+  // window.logout = logout;
+  // window.signup = signup;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
 });
--- Instaclone/frontend/reducers/session_reducer.js
+++ Instaclone/frontend/reducers/session_reducer.js
@@ -2,17 +2,19 @@ import merge from 'lodash/merge';
 
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
-const _nullUser = Object.freeze({
+const _nullSession = Object.freeze({
   currentUser: null,
   // errors: []
 });
 
-const sessionReducer = (state = _nullUser, action) => {
+const sessionReducer = (state = _nullSession, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
       const currentUser = action.currentUser;
       return merge({}, { currentUser });
+    // case LOGOUT_CURRENT_USER:
+    //   return _nullSession;
     default:
       return state;
   }
