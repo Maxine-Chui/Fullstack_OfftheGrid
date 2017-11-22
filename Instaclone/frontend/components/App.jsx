5a17a4a39fdf8433b8d4b5671641d361440996dc
Maxine Chui <maxine.chui@gmail.com>
2017-11-21 22:06:56 -0800

    finished SessionForm

--- Instaclone/frontend/components/App.jsx
+++ Instaclone/frontend/components/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
 
 const App = () => (
   <div>
@@ -7,6 +15,9 @@ const App = () => (
       <h1>Instaclone</h1>
       <GreetingContainer />
     </header>
+
+    <Route path="/login" component={SessionFormContainer} />
+    <Route path="/signup" component={SessionFormContainer} />
   </div>
 );
 
--- Instaclone/frontend/instaclone.jsx
+++ Instaclone/frontend/instaclone.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, signup, logout } from './util/session_api_util';
+import { login, signup, logout } from './actions/session_actions';
 import configureStore from './store/store';
 import Root from './components/root';
 
@@ -9,9 +9,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
   // console.log("in doc ready");
-  // window.login = login;
-  // window.logout = logout;
-  // window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
 });
