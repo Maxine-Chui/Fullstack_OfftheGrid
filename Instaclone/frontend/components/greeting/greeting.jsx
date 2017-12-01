5d4dfc42f90360228e9226787ba9a0a1af5492cb
Maxine Chui <maxine.chui@gmail.com>
2017-12-01 13:35:38 -0800

    touch up styling

--- Instaclone/frontend/components/greeting/greeting.jsx
+++ Instaclone/frontend/components/greeting/greeting.jsx
@@ -18,38 +18,31 @@ const personalGreeting = (currentUser, logout) => {
           <Link to="/posts"><div className="logo"><i className="fa fa-instagram" aria-hidden="true"></i></div></Link>
           <Link to="/posts"><div id="nav-name">OfftheGrid</div></Link>
         </div>
-        <div className="nav-search-bar">
-          <div className="search-logo"><i className="fa fa-search"></i></div>
-          <label>
-            <input type="text"
-              placeholder=""
-              className="nav-search-input"
-              />
-          </label>
-        </div>
         <div className="nav-button-group">
           <Link to={"/upload"}><span><i className="fa fa-plus" aria-hidden="true"></i></span></Link>
           <Link to={`/users/${currentUser.id}`}><span><i className="fa fa-user-o" aria-hidden="true"></i></span></Link>
           <span><i className="fa fa-sign-out" onClick={logout} aria-hidden="true"></i></span>
         </div>
-      </div>
+    </div>
     </hgroup>
 
   );
 };
-//   <h2 className="header-name">Hi, {currentUser.username}!</h2>
-//   <button className="header-button" onClick={logout}>Log Out</button>
 
 const Greeting = ({ currentUser, logout }) => (
-  // {
-  // if (currentUser) {
-  //   return personalGreeting(currentUser, logout);
-  // }
-// }
   currentUser ? personalGreeting(currentUser, logout) : null
-  // sessionLinks()
 );
-// onClick={() => linkToProfile(currentUser.id)}
+
 export default Greeting;
 
-// .then(() => this.props.history.push('/login'))
+
+// </div>
+// <div className="nav-search-bar">
+//   <div className="search-logo"><i className="fa fa-search"></i></div>
+//   <label>
+//     <input type="text"
+//       placeholder=""
+//       className="nav-search-input"
+//       />
+//   </label>
+// </div>
