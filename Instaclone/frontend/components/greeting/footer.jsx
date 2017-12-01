307bee369ae9ecc1d9a600579a9e46ecbae32ed8
Maxine Chui <maxine.chui@gmail.com>
2017-12-01 14:05:12 -0800

    touch up footer styling

--- Instaclone/frontend/components/greeting/footer.jsx
+++ Instaclone/frontend/components/greeting/footer.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="website-links">
-        <div className="github"><a href="https://github.com/Maxine-Chui">Github</a></div>
-        <div className="linkedin"><a href="https://www.linkedin.com/in/maxinechui/">LinkedIn</a></div>
+    <div className="footer-container">
+      <div className="footer">
+        <div className="website-links">
+          <div className="github"><a href="https://github.com/Maxine-Chui">Github</a></div>
+          <div className="linkedin"><a href="https://www.linkedin.com/in/maxinechui/">LinkedIn</a></div>
+        </div>
+        <div className="email">maxine.chui@berkeley.edu</div>
       </div>
-      <div className="email">maxine.chui@berkeley.edu</div>
     </div>
   );
 };
