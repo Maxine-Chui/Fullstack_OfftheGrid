041b954b225b846138f26891189a45968cf7e807
Maxine Chui <maxine.chui@gmail.com>
2018-01-17 20:17:20 -0800

    add loading icon

--- Instaclone/frontend/reducers/loading_reducer.js
+++ Instaclone/frontend/reducers/loading_reducer.js
@@ -4,12 +4,22 @@ import {
   REMOVE_POST
 } from '../actions/posts_actions';
 
+import merge from 'lodash/merge';
+
 const _initialState = {
-  loading: false
+  loading: true
 };
 
 const loadingReducer = (state = _initialState, action) => {
-
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_POSTS:
+      return merge({}, state, { loading: false });
+    // case RECEIVE_POST:
+    //   return merge({}, state, { loading: false });
+    default:
+      return state;
+  }
 };
 
 export default loadingReducer;
